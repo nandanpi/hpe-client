ab9e331d6c712ff163b0261607b8947f969dcd89
Nandan R Pai <nandanrp09@gmail.com>
2025-06-12 15:57:04 +0530

    minor fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Urlanaysis from "./components/url-analysis";
 export default function HomePage() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <div className="bg-background text-foreground min-h-screen space-y-4 px-4 py-4">
         <section className="max-w-8xl bg-card border-border mx-auto rounded-xl border p-6 shadow-sm">
           <Urlanaysis />
@@ -18,7 +18,7 @@ export default function HomePage() {
             throw new Error("Function not implemented.");
           }}
         />
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </>
   );
